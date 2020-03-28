cf8a6f1422b58f975466e4ca7c4906b394e3ada9
Chad Crowe <ccrowe@ccrowe-mn1.linkedin.biz>
2020-03-27 18:07:37 -0700

    tested file successfully

---
+++ hello.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let input_array = vec![2, 7, 11, 15];
+    let target = 9;
+    let answer = two_sum(input_array, target);
+    println!("{:?}", answer);
+}
+
+pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<usize> {
+    for index1 in 0..nums.len(){
+        for index2 in index1..nums.len(){
+            let to_add1 = nums[index1];
+            let to_add2 = nums[index2];
+            if to_add1 + to_add2 == target {
+                return vec![index1, index2]
+            }
+        }
+    }
+    return vec![0,0];
+}
+
